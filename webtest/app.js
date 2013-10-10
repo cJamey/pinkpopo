2c0c94519ea0f8eb8b5e504ed56b885d30a428c0
cjamey <caijia288@gmail.com>
2013-10-10 17:49:41 +0800

    change the content

--- webtest/app.js
+++ webtest/app.js
@@ -6,6 +6,8 @@ http.createServer(function(req,res){
 	res.writeHead(200,{'Content-Type':'text/html'});
 	res.write('<h1>Node.js</h1>');
 	res.end('<p>Hello World</p>');
+	res.write('<h1>Pinkpopo的世界</h1>');
+	res.end('<p>等待pinkpopo更新最新的款式</p>');
 }).listen(3000);
 
 console.log("Http Server Is Listening at Port 3000.");
